fae546510b46479f63160625a6c32f1daeb39db9
Byron Henze <byronh@gmail.com>
2015-12-26 23:05:25 -0800

    Add toolbar

--- app/src/main/java/com/byronhenze/homecontrol/MainActivity.java
+++ app/src/main/java/com/byronhenze/homecontrol/MainActivity.java
@@ -1,14 +1,40 @@
 package com.byronhenze.homecontrol;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setTitle(R.string.app_title);
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)this.findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
+        this.getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this.getApplicationContext(), "Settings!", Toast.LENGTH_SHORT).show();
+//                this.startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
