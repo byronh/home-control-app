95a42af86b716dc01cfd6d326a9e75bffb550ad1
Byron Henze <byronh@gmail.com>
2015-12-27 21:12:10 -0800

    Implement preferences

--- app/src/main/java/com/byronhenze/homecontrol/SettingsFragment.java
+++ app/src/main/java/com/byronhenze/homecontrol/SettingsFragment.java
@@ -1,14 +1,51 @@
 package com.byronhenze.homecontrol;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
+
+    WifiBroadcastReceiver receiver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.addPreferencesFromResource(R.xml.preferences);
+    }
 
-        addPreferencesFromResource(R.xml.preferences);
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences preferences = this.getPreferenceManager().getSharedPreferences();
+        preferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        SharedPreferences preferences = this.getPreferenceManager().getSharedPreferences();
+        preferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Context context = this.getActivity().getApplicationContext();
+        if (key.equals(this.getString(R.string.pref_key_wifi))) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            if (preferences.getBoolean(key, false)) {
+                IntentFilter intentFilter = new IntentFilter();
+                intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+                receiver = new WifiBroadcastReceiver();
+                context.registerReceiver(this.receiver, intentFilter);
+            } else if (receiver != null) {
+                context.unregisterReceiver(this.receiver);
+            }
+        }
     }
 }
--- app/src/main/java/com/byronhenze/homecontrol/WifiBroadcastReceiver.java
+++ app/src/main/java/com/byronhenze/homecontrol/WifiBroadcastReceiver.java
@@ -3,13 +3,23 @@ package com.byronhenze.homecontrol;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
 
 public class WifiBroadcastReceiver extends BroadcastReceiver {
-    public WifiBroadcastReceiver() {
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        ConnectivityManager manager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = manager.getActiveNetworkInfo();
+        if (activeNetwork != null && activeNetwork.getType() == ConnectivityManager.TYPE_WIFI && activeNetwork.isConnected()) {
+            WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifi = wifiManager.getConnectionInfo();
+            String bssid = wifi.getBSSID();
+            Log.e("HomeControl", "Connected to BSSID: " + bssid);
+        }
     }
 }
